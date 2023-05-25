9d11b94e4ab9c693b320394caf54e8c7d03238f0
Thanh Hieu <hieudoan280102@gmail.com>
2023-05-25 19:50:24 +0700

    them vao

--- DBScript.sql
+++ DBScript.sql
@@ -1,4 +1,5 @@
-﻿
+﻿	USE master
+	GO
 	DROP DATABASE IF EXISTS VHomeClean;
 	GO
 	CREATE DATABASE VHomeClean;
@@ -64,7 +65,7 @@ CREATE TABLE Account
   Address NVARCHAR(255) NOT NULL,
   Phone NVARCHAR(20) NOT NULL,
   RoleID INT NOT NULL,
-  DateOfBirth NVARCHAR(100) NOT NULL,
+  DateOfBirth DATE NOT NULL,
   Status BIT NOT NULL,
   Image NVARCHAR(255) NOT NULL,
   Salary MONEY,
@@ -189,11 +190,20 @@ END;
 
 -----------------------INSERT DATA TO TABLE(NHỚ INSERT DỮ LIỆU THEO THỨ TỰ BẢNG ĐƯỢC TẠO Ở CODE TRÊN) --------------------------------------------------
 
-INSERT INTO RoleAccount (RoleID,RoleName)
-VALUES(1,'ADMIN');
+INSERT INTO RoleAccount (RoleID, RoleName)
+VALUES
+    (1, 'ADMIN'),
+    (0, 'USER');
 
-INSERT INTO RoleAccount (RoleID,RoleName)
-VALUES(0,'User');
+
+
+
+
+
+
+
+
+
 
 
 
--- V-HomeClean/src/java/DTO/UserError.java
+++ V-HomeClean/src/java/DTO/UserError.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package users;
+package DTO;
 
 /**
  *
  * @author Asus
  */
-public class UserDAO {
+public class UserError {
     
 }
