c360b2b539893b38ff902481be6f2f65b1a49600
Xuan Quy <hoangxuanquy5@gmail.com>
2023-07-05 21:27:00 +0700

    pull

---
+++ V-HomeClean/build/generated/src/org/apache/jsp/contact_jsp.java
@@ -0,0 +1,274 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class contact_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_forEach_var_items;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_c_forEach_var_items = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_c_forEach_var_items.release();
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<!DOCTYPE html>\r\n");
+      out.write("<html lang=\"en\">\r\n");
+      out.write("\r\n");
+      out.write("    <head>\r\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\r\n");
+      out.write("        <meta charset=\"utf-8\">\r\n");
+      out.write("        <title>V-HomeClean - Liên Hệ</title>\r\n");
+      out.write("        <meta content=\"width=device-width, initial-scale=1.0\" name=\"viewport\">\r\n");
+      out.write("        <meta content=\"\" name=\"keywords\">\r\n");
+      out.write("        <meta content=\"\" name=\"description\">\r\n");
+      out.write("\r\n");
+      out.write("        <!-- Favicon -->\r\n");
+      out.write("        <link href=\"img/favicon.ico\" rel=\"icon\">\r\n");
+      out.write("\r\n");
+      out.write("        <!-- Google Web Fonts -->\r\n");
+      out.write("        <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\r\n");
+      out.write("        <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\r\n");
+      out.write("        <!--        <link href=\"https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Poppins:wght@600;700&display=swap\" rel=\"stylesheet\"> -->\r\n");
+      out.write("        <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\r\n");
+      out.write("        <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\r\n");
+      out.write("        <link href=\"https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap\" rel=\"stylesheet\">\r\n");
+      out.write("\r\n");
+      out.write("        <!-- Icon Font Stylesheet -->\r\n");
+      out.write("        <link href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css\" rel=\"stylesheet\">\r\n");
+      out.write("        <link href=\"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css\" rel=\"stylesheet\">\r\n");
+      out.write("\r\n");
+      out.write("        <!-- Libraries Stylesheet -->\r\n");
+      out.write("        <link href=\"lib/animate/animate.min.css\" rel=\"stylesheet\">\r\n");
+      out.write("        <link href=\"lib/owlcarousel/assets/owl.carousel.min.css\" rel=\"stylesheet\">\r\n");
+      out.write("\r\n");
+      out.write("        <!-- Customized Bootstrap Stylesheet -->\r\n");
+      out.write("        <link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">\r\n");
+      out.write("\r\n");
+      out.write("        <!-- Template Stylesheet -->\r\n");
+      out.write("        <link href=\"css/style.css\" rel=\"stylesheet\">\r\n");
+      out.write("    </head>\r\n");
+      out.write("\r\n");
+      out.write("    <body>\r\n");
+      out.write("        ");
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "navigation.jsp", out, false);
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("            <!-- Page Header Start -->\r\n");
+      out.write("            <div class=\"container-fluid page-header py-5 mb-5 wow fadeIn\" data-wow-delay=\"0.1s\">\r\n");
+      out.write("                <div class=\"container text-center py-5\">\r\n");
+      out.write("                    <h1 class=\"display-4 text-white animated slideInDown mb-4\">Liên Hệ Với Chúng Tôi</h1>\r\n");
+      out.write("                    <nav aria-label=\"breadcrumb animated slideInDown\">\r\n");
+      out.write("                        <ol class=\"breadcrumb justify-content-center mb-0\">\r\n");
+      out.write("                            <li class=\"breadcrumb-item\"><a class=\"text-white\" href=\"HomePageController\">Trang Chủ</a></li>\r\n");
+      out.write("                            <li class=\"breadcrumb-item text-primary active\" aria-current=\"page\">Liên Hệ</li>\r\n");
+      out.write("                        </ol>\r\n");
+      out.write("                    </nav>\r\n");
+      out.write("                </div>\r\n");
+      out.write("            </div>\r\n");
+      out.write("            <!-- Page Header End -->\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("            <!-- Contact Start -->\r\n");
+      out.write("            <div class=\"container-xxl py-5\">\r\n");
+      out.write("                <div class=\"container\">\r\n");
+      out.write("                    <div class=\"row g-5\">\r\n");
+      out.write("                    ");
+      if (_jspx_meth_c_forEach_0(_jspx_page_context))
+        return;
+      out.write("\r\n");
+      out.write("                    <div class=\"col-lg-6 wow fadeInUp\" data-wow-delay=\"0.1s\" style=\"min-height: 450px;\">\r\n");
+      out.write("                        <div class=\"position-relative h-100\">\r\n");
+      out.write("                            <iframe class=\"position-relative w-100 h-100\"\r\n");
+      out.write("                                    src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd\"\r\n");
+      out.write("                                    frameborder=\"0\" style=\"min-height: 450px; border:0;\" allowfullscreen=\"\" aria-hidden=\"false\"\r\n");
+      out.write("                                    tabindex=\"0\"></iframe>\r\n");
+      out.write("                        </div>\r\n");
+      out.write("                    </div>\r\n");
+      out.write("                    <div class=\"col-lg-6 wow fadeInUp\" data-wow-delay=\"0.5s\">\r\n");
+      out.write("                        <div class=\"border-start border-5 border-primary ps-4 mb-5\">\r\n");
+      out.write("                            <h6 class=\"text-body text-uppercase mb-2\">Contact Us</h6>\r\n");
+      out.write("                            <h1 class=\"display-6 mb-0\">Hãy điền thông tin để chúng tôi có thể hỗ trợ bạn nhé</h1>\r\n");
+      out.write("                        </div>\r\n");
+      out.write("                        <p class=\"mb-4\">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href=\"https://htmlcodex.com/contact-form\">Download Now</a>.</p>\r\n");
+      out.write("                        <form>\r\n");
+      out.write("                            <div class=\"row g-3\">\r\n");
+      out.write("                                <div class=\"col-md-6\">\r\n");
+      out.write("                                    <div class=\"form-floating\">\r\n");
+      out.write("                                        <input type=\"text\" class=\"form-control border-0 bg-light\" id=\"name\" placeholder=\"Your Name\">\r\n");
+      out.write("                                        <label for=\"name\">Your Name</label>\r\n");
+      out.write("                                    </div>\r\n");
+      out.write("                                </div>\r\n");
+      out.write("                                <div class=\"col-md-6\">\r\n");
+      out.write("                                    <div class=\"form-floating\">\r\n");
+      out.write("                                        <input type=\"email\" class=\"form-control border-0 bg-light\" id=\"email\" placeholder=\"Your Email\">\r\n");
+      out.write("                                        <label for=\"email\">Your Email</label>\r\n");
+      out.write("                                    </div>\r\n");
+      out.write("                                </div>\r\n");
+      out.write("                                <div class=\"col-12\">\r\n");
+      out.write("                                    <div class=\"form-floating\">\r\n");
+      out.write("                                        <input type=\"text\" class=\"form-control border-0 bg-light\" id=\"subject\" placeholder=\"Subject\">\r\n");
+      out.write("                                        <label for=\"subject\">Subject</label>\r\n");
+      out.write("                                    </div>\r\n");
+      out.write("                                </div>\r\n");
+      out.write("                                <div class=\"col-12\">\r\n");
+      out.write("                                    <div class=\"form-floating\">\r\n");
+      out.write("                                        <textarea class=\"form-control border-0 bg-light\" placeholder=\"Leave a message here\" id=\"message\" style=\"height: 150px\"></textarea>\r\n");
+      out.write("                                        <label for=\"message\">Message</label>\r\n");
+      out.write("                                    </div>\r\n");
+      out.write("                                </div>\r\n");
+      out.write("                                <div class=\"col-12\">\r\n");
+      out.write("                                    <button class=\"btn btn-primary py-3 px-5\" type=\"submit\">Send Message</button>\r\n");
+      out.write("                                </div>\r\n");
+      out.write("                            </div>\r\n");
+      out.write("                        </form>\r\n");
+      out.write("                    </div>\r\n");
+      out.write("                </div>\r\n");
+      out.write("            </div>\r\n");
+      out.write("        </div>\r\n");
+      out.write("        <!-- Contact End -->\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("        <!-- Footer Start -->\r\n");
+      out.write("        ");
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "footer.jsp", out, false);
+      out.write("\r\n");
+      out.write("        <!-- Footer End -->\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("        <!-- Back to Top -->\r\n");
+      out.write("        <a href=\"#\" class=\"btn btn-lg btn-primary btn-lg-square back-to-top\"><i class=\"bi bi-arrow-up\"></i></a>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("        <!-- JavaScript Libraries -->\r\n");
+      out.write("        <script src=\"https://code.jquery.com/jquery-3.4.1.min.js\"></script>\r\n");
+      out.write("        <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js\"></script>\r\n");
+      out.write("        <script src=\"lib/wow/wow.min.js\"></script>\r\n");
+      out.write("        <script src=\"lib/easing/easing.min.js\"></script>\r\n");
+      out.write("        <script src=\"lib/waypoints/waypoints.min.js\"></script>\r\n");
+      out.write("        <script src=\"lib/owlcarousel/owl.carousel.min.js\"></script>\r\n");
+      out.write("\r\n");
+      out.write("        <!-- Template Javascript -->\r\n");
+      out.write("        <script src=\"js/main.js\"></script>\r\n");
+      out.write("    </body>\r\n");
+      out.write("\r\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+
+  private boolean _jspx_meth_c_forEach_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:forEach
+    org.apache.taglibs.standard.tag.rt.core.ForEachTag _jspx_th_c_forEach_0 = (org.apache.taglibs.standard.tag.rt.core.ForEachTag) _jspx_tagPool_c_forEach_var_items.get(org.apache.taglibs.standard.tag.rt.core.ForEachTag.class);
+    _jspx_th_c_forEach_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_forEach_0.setParent(null);
+    _jspx_th_c_forEach_0.setItems((java.lang.Object) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${ListA}", java.lang.Object.class, (PageContext)_jspx_page_context, null));
+    _jspx_th_c_forEach_0.setVar("o");
+    int[] _jspx_push_body_count_c_forEach_0 = new int[] { 0 };
+    try {
+      int _jspx_eval_c_forEach_0 = _jspx_th_c_forEach_0.doStartTag();
+      if (_jspx_eval_c_forEach_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+        do {
+          out.write("\r\n");
+          out.write("                        <div class=\"col-lg-6 wow fadeInUp\" data-wow-delay=\"0.1s\">\r\n");
+          out.write("                            <div class=\"row g-4 align-items-center\">\r\n");
+          out.write("                                <div class=\"col-sm-6\">\r\n");
+          out.write("                                    <img class=\"img-fluid\" src=\"");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${o.image}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("\" alt=\"\">\r\n");
+          out.write("                                </div>\r\n");
+          out.write("                                <div class=\"col-sm-6\">\r\n");
+          out.write("                                    <h3 class=\"mb-0\">");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${o.fullName}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("</h3>\r\n");
+          out.write("                                    <p>Head of Project</p>\r\n");
+          out.write("                                    <h6>Thông tin liên lạc:</h6>\r\n");
+          out.write("                                    <p>");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${o.address}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("</p>\r\n");
+          out.write("                                    <p class=\"mb-0\">Call: ");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${o.phone}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("</p>\r\n");
+          out.write("                                    <p class=\"mb-0\">Email: ");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${o.email}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+          out.write("</p>\r\n");
+          out.write("                                </div>\r\n");
+          out.write("                            </div>\r\n");
+          out.write("                        </div>\r\n");
+          out.write("                    ");
+          int evalDoAfterBody = _jspx_th_c_forEach_0.doAfterBody();
+          if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+            break;
+        } while (true);
+      }
+      if (_jspx_th_c_forEach_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_c_forEach_0[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_c_forEach_0.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_c_forEach_0.doFinally();
+      _jspx_tagPool_c_forEach_var_items.reuse(_jspx_th_c_forEach_0);
+    }
+    return false;
+  }
+}
